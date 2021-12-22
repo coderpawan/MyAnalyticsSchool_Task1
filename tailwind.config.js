ced8aa09f3fe13d3cc6e1bfb5b91118cb5ac281b
coderpawan <sahupawan9749568594@gmail.com>
2021-12-22 15:21:57 +0530

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,7 @@
-const colors = require("tailwindcss/colors");
-
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        cyan: colors.cyan,
-        teal: colors.teal,
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
